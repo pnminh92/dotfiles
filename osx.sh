f81c8f8ebd564c4484576f0ccb21f38554190d1a
Minh Phan <wofi.minh@1pac.vn>
2017-12-25 10:08:27 +0700

    Update mac osx setup

--- osx.sh
+++ osx.sh
@@ -4,11 +4,13 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" # brew
 brew tap caskroom/cask
 
+defaults write -g KeyRepeat -float 1.4
+defaults write -g InitialKeyRepeat -int 12
+
 brew cask install macdown
 brew cask install google-chrome
 brew cask install sequel-pro
 brew cask install psequel
-brew cask install karabiner
 brew cask install virtualbox
 brew cask install vagrant
 brew cask install the-unarchiver
@@ -25,19 +27,16 @@ brew install python
 brew install nginx
 brew install mysql
 brew install postgresql
+brew install redis
 brew install the_silver_searcher
 brew install neovim/neovim/neovim
 brew install iterm2
 brew install tmux
 brew install zsh
 brew install ctags
+brew install tree
 brew install yarn
-
-npm install -g gulp-cli
-npm install -g grunt-cli
-npm install -g lodash-cli
-npm install -g vue-cli
-npm install -g yarn
+brew install hub
 
 gem install bundler
 gem install tmuxinator
@@ -45,14 +44,5 @@ gem install sass
 gem install scss-lint
 gem install iStats
 
-# copy dotfiles
-git clone https://github.com/wofi-minh/dotfiles.git ~/Projects/dotfiles
-cd ~/Projects/dotfiles
-cp -r .vimrc .tmux.conf .agignore .gitconfig .gitignore_global .vim ~/
-cp .zshrc_osx ~/.zshrc
-mkdir -p ~/.config
-ln -s ~/.vim ~/.config/nvim
-ln -s ~/.vimrc ~/.config/nvim/init.vim
-
-# oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
